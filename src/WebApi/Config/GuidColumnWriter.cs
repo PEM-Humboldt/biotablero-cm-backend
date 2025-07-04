a0324411aa34e38e56aa4918cbc2a11e79e649d1
cgalvis <cgalvis@humboldt.org.co>
2025-07-04 11:08:48 -0500

    refactor: added changes in custom column writers

--- src/WebApi/Config/GuidColumnWriter.cs
+++ src/WebApi/Config/GuidColumnWriter.cs
@@ -7,16 +7,21 @@ using NpgsqlTypes;
 using Serilog.Events;
 using Serilog.Sinks.PostgreSQL;
 
-public class GuidColumnWriter : ColumnWriterBase
+/// <summary>
+/// Custom Serilog column writer for GUID data
+/// </summary>
+/// <param name="propertyName">Serilog property name</param>
+/// <param name="dbType">Column database type</param>
+public class GuidColumnWriter(string propertyName, NpgsqlDbType dbType = NpgsqlDbType.Uuid) : ColumnWriterBase(dbType)
 {
-    private readonly string propertyName;
+    private readonly string propertyName = propertyName;
 
-    public GuidColumnWriter(string propertyName, NpgsqlDbType dbType = NpgsqlDbType.Uuid)
-        : base(dbType)
-    {
-        this.propertyName = propertyName;
-    }
-
+    /// <summary>
+    /// Get data value
+    /// </summary>
+    /// <param name="logEvent">Log event data</param>
+    /// <param name="formatProvider">Format provider</param>
+    /// <returns>Scalar property value</returns>
     public override object GetValue(LogEvent logEvent, IFormatProvider formatProvider = null)
     {
         if (logEvent.Properties.TryGetValue(propertyName, out var value))
--- src/WebApi/Config/RawStringColumnWriter.cs
+++ src/WebApi/Config/RawStringColumnWriter.cs
@@ -7,16 +7,24 @@ using NpgsqlTypes;
 using Serilog.Events;
 using Serilog.Sinks.PostgreSQL;
 
-public class RawStringColumnWriter : ColumnWriterBase
+/// <summary>
+/// Custom Serilog column writer for strings
+/// </summary>
+/// <remarks>
+/// Constructor
+/// </remarks>
+/// <param name="propertyName">Serilog property name</param>
+/// <param name="dbType">Column database type</param>
+public class RawStringColumnWriter(string propertyName, NpgsqlDbType dbType = NpgsqlDbType.Text) : ColumnWriterBase(dbType)
 {
-    private readonly string propertyName;
+    private readonly string propertyName = propertyName;
 
-    public RawStringColumnWriter(string propertyName, NpgsqlDbType dbType = NpgsqlDbType.Text)
-        : base(dbType)
-    {
-        this.propertyName = propertyName;
-    }
-
+    /// <summary>
+    /// Get data value
+    /// </summary>
+    /// <param name="logEvent">Log event data</param>
+    /// <param name="formatProvider">Format provider</param>
+    /// <returns>Scalar property value</returns>
     public override object GetValue(LogEvent logEvent, IFormatProvider formatProvider = null)
     {
         if (logEvent.Properties.TryGetValue(propertyName, out var value))
@@ -25,6 +33,7 @@ public class RawStringColumnWriter : ColumnWriterBase
             {
                 return scalar.Value?.ToString();
             }
+
             return value.ToString();
         }
 
