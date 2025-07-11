10637db74f26e3a9eab8e8982fbfeda2cc9b0568
cgalvis <cgalvis@humboldt.org.co>
2025-07-11 16:34:02 -0500

    refactor: added comments in mappings configuration class

--- src/WebApi/Config/ConfigMappings.cs
+++ src/WebApi/Config/ConfigMappings.cs
@@ -5,11 +5,18 @@ using IAVH.BioTablero.CM.Application.Mappings;
 using IAVH.BioTablero.CM.Core.DTOs.LogNS;
 using IAVH.BioTablero.CM.Core.Entities.LogNS;
 
-
 using Microsoft.Extensions.DependencyInjection;
 
+/// <summary>
+/// Mappings configuration
+/// </summary>
 public static class ConfigMappings
 {
+    /// <summary>
+    /// Add system mappings
+    /// </summary>
+    /// <param name="services">Application services</param>
+    /// <returns>Host builder configuration</returns>
     public static IServiceCollection AddMappings(this IServiceCollection services)
     {
         services.AddScoped<IMapper<LogEntity, LogDto>, LogMappings>();
