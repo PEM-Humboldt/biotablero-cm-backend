a484e2fc7f01a02d8ee5020292171a380bef2623
cgalvis <cgalvis@humboldt.org.co>
2025-07-21 16:45:35 -0500

    feat: added first migration

---
+++ src/Persistence/Migrations/20250721213943_InitialMigration.cs
@@ -0,0 +1,44 @@
+﻿#nullable disable
+
+namespace IAVH.BioTablero.CM.Persistence.Migrations;
+
+using System;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+/// <inheritdoc />
+public partial class InitialMigration : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.EnsureSchema(
+            name: "logs");
+
+        migrationBuilder.CreateTable(
+            name: "logs",
+            schema: "logs",
+            columns: table => new
+            {
+                id = table.Column<Guid>(type: "uuid", nullable: false),
+                timestamp = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                level = table.Column<int>(type: "integer", nullable: false),
+                type = table.Column<int>(type: "integer", nullable: true),
+                message = table.Column<string>(type: "text", nullable: true),
+                user_name = table.Column<string>(type: "text", nullable: true),
+                custom_record = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                client_ip = table.Column<string>(type: "text", nullable: true),
+                client_agent = table.Column<string>(type: "text", nullable: true),
+                properties = table.Column<string>(type: "jsonb", nullable: false),
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_logs", x => x.id);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropTable(
+            name: "logs",
+            schema: "logs");
+}
