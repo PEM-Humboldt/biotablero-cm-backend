9e0294899bde37903f34bef7a5a3e4d09240a1ad
cgalvis <cgalvis@humboldt.org.co>
2025-08-05 15:33:57 -0500

    feat: deleted unused initiative user level dto

--- src/Application/DTOs/Initiatives/InitiativeUserDto.cs
+++ src/Application/DTOs/Initiatives/InitiativeUserDto.cs
@@ -1,6 +1,9 @@
 ﻿namespace IAVH.BioTablero.CM.Application.DTOs.Initiatives;
 
+using IAVH.BioTablero.CM.Application.DTOs.Utils;
 using IAVH.BioTablero.CM.Application.Interfaces.General;
+
+using static IAVH.BioTablero.CM.Core.Domain.Utils.Enums.InitiativesEnums;
 
 /// <summary>
 /// Initiative User entity
@@ -23,12 +26,7 @@ public class InitiativeUserDto : IDto
     public int UserId { get; set; }
 
     /// <summary>
-    /// Level identifier
-    /// </summary>
-    public int LevelId { get; set; }
-
-    /// <summary>
     /// Level relationship
     /// </summary>
-    public InitiativeUserLevelDto Level { get; set; }
+    public EnumEntityDto<InitiativeUserLevel> Level { get; set; }
 }
