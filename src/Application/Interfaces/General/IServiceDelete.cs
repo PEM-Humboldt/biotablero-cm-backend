b1458fa5e7ea59833293c43a7fe382f075f1a292
cgalvis <cgalvis@humboldt.org.co>
2025-08-08 13:33:36 -0500

    feat: added delete service interface

---
+++ src/Application/Interfaces/General/IServiceDelete.cs
@@ -0,0 +1,22 @@
+﻿namespace IAVH.BioTablero.CM.Application.Interfaces.General;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using IAVH.BioTablero.CM.Application.Utils;
+
+/// <summary>
+/// Delete service interface.
+/// </summary>
+/// <typeparam name="TEntityType">Entity identifier type.</typeparam>
+public interface IServiceDelete<TEntityType>
+    where TEntityType : notnull
+{
+    /// <summary>
+    /// Delete element.
+    /// </summary>
+    /// <param name="id">Element identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Process result.</returns>
+    Task<CustomWebResponse> Delete(TEntityType id, CancellationToken ct = default);
+}
