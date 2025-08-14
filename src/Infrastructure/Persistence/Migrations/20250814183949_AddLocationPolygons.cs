6d14f854c206d98a4319fa7df6f15e1aabbbf33b
cgalvis <cgalvis@humboldt.org.co>
2025-08-14 13:42:25 -0500

    feat: added migration for location geometry

---
+++ src/Infrastructure/Persistence/Migrations/20250814183949_AddLocationPolygons.cs
@@ -0,0 +1,49 @@
+﻿#nullable disable
+
+namespace IAVH.BioTablero.CM.Infrastructure.Persistence.Migrations;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+using NetTopologySuite.Geometries;
+
+/// <inheritdoc />
+public partial class AddLocationPolygons : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterDatabase()
+            .Annotation("Npgsql:PostgresExtension:postgis", ",,");
+
+        migrationBuilder.CreateTable(
+            name: "location_polygons",
+            schema: "geo",
+            columns: table => new
+            {
+                id = table.Column<int>(type: "integer", nullable: false),
+                geometry = table.Column<Geometry>(type: "geometry(Polygon, 4326)", nullable: false),
+                geometry_simplified = table.Column<string>(type: "text", nullable: false),
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_location_polygons", x => x.id);
+                table.ForeignKey(
+                    name: "FK_location_polygons_location_id",
+                    column: x => x.id,
+                    principalSchema: "geo",
+                    principalTable: "location",
+                    principalColumn: "id");
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "location_polygons",
+            schema: "geo");
+
+        migrationBuilder.AlterDatabase()
+            .OldAnnotation("Npgsql:PostgresExtension:postgis", ",,");
+    }
+}
