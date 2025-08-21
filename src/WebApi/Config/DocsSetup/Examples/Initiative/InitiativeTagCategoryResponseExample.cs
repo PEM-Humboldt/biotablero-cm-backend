64091697dd7dbb0ff40337ebe6e01cd78a19d5ad
cgalvis <cgalvis@humboldt.org.co>
2025-08-21 12:05:36 -0500

    feat: added initiative tag category controller

---
+++ src/WebApi/Config/DocsSetup/Examples/Initiative/InitiativeTagCategoryResponseExample.cs
@@ -0,0 +1,23 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.Initiative;
+
+using System.Collections.Generic;
+
+using IAVH.BioTablero.CM.Application.DTOs.Utils;
+using IAVH.BioTablero.CM.Application.Interfaces.General;
+
+using Swashbuckle.AspNetCore.Filters;
+
+using static IAVH.BioTablero.CM.Core.Domain.Utils.Enums.InitiativesEnums;
+
+/// <summary>
+/// Initiative Tag Category response example.
+/// </summary>
+/// <param name="entityService">General enumeration service.</param>
+public class InitiativeTagCategoryResponseExample(IServiceReadEnumeration<InitiativeTagCategory> entityService) : IExamplesProvider<IEnumerable<EnumEntityDto<InitiativeTagCategory>>>
+{
+    /// <summary>
+    /// Get examples for entity.
+    /// </summary>
+    /// <returns>Entity examples.</returns>
+    public IEnumerable<EnumEntityDto<InitiativeTagCategory>> GetExamples() => entityService.GetEnumerable();
+}
---
+++ src/WebApi/Controllers/Rest/Initiatives/InitiativeTagCategoryController.cs
@@ -0,0 +1,39 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Controllers.Rest.Initiatives;
+
+using IAVH.BioTablero.CM.Application.Interfaces.General;
+using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.Initiative;
+
+using IAVH.BioTablero.CM.WebApi.Interfaces;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Swashbuckle.AspNetCore.Filters;
+
+using static IAVH.BioTablero.CM.Core.Domain.Utils.Enums.InitiativesEnums;
+
+/// <summary>
+/// Initiative Tag Category controller.
+/// </summary>
+/// <param name="webTools">General web tools.</param>
+/// <param name="entityService">Entity service.</param>
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+public class InitiativeTagCategoryController(IWebTools webTools,
+    IServiceReadEnumeration<InitiativeTagCategory> entityService) : ControllerBase
+{
+    /// <summary>
+    /// Get all entities.
+    /// </summary>
+    /// <returns>Entities list from parameters.</returns>
+    [HttpGet]
+    [ProducesResponseType(typeof(InitiativeTagCategoryResponseExample), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(InitiativeTagCategoryResponseExample))]
+    public IActionResult Get()
+    {
+        var response = entityService.GetAll();
+        return webTools.CustomResponse(response);
+    }
+}
