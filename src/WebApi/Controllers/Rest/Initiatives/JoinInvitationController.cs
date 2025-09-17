f3ffce2f9e42ede4b691be295c25121db151196e
cgalvis <cgalvis@humboldt.org.co>
2025-09-17 17:10:00 -0500

    feat: added join invitation controller

---
+++ src/WebApi/Controllers/Rest/Initiatives/JoinInvitationController.cs
@@ -0,0 +1,57 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Controllers.Rest.Initiatives;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using IAVH.BioTablero.CM.Application.DTOs.Initiatives;
+using IAVH.BioTablero.CM.Application.Interfaces.Services;
+using IAVH.BioTablero.CM.Core.Domain.Entities.Initiatives;
+using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples;
+using IAVH.BioTablero.CM.WebApi.Interfaces;
+using IAVH.BioTablero.CM.WebApi.Utils;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+/// <summary>
+/// Join Invitation controller.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+[ApiConventionType(typeof(CustomApiConventions))]
+public class JoinInvitationController(
+    IWebTools webTools,
+    IJoinInvitationService entityService) : ControllerBase
+{
+    /// <summary>
+    /// Get entities (paginated).
+    /// </summary>
+    /// <param name="queryOptions">OData query options.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Entities list from parameters.</returns>
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> GetOdataList(ODataQueryOptions<JoinInvitation> queryOptions, CancellationToken ct)
+    {
+        var response = await entityService.GetListAsync(queryOptions, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Add entity.
+    /// </summary>
+    /// <param name="requestData">Request data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Added entity data.</returns>
+    [HttpPut]
+    [Authorize]
+    public async Task<IActionResult> Put([FromBody] JoinInvitationDto requestData, CancellationToken ct)
+    {
+        requestData.Creator = HttpContext.GetUserName();
+        var response = await entityService.AddAsync(requestData, ct);
+        return webTools.CustomResponse(response);
+    }
+}
