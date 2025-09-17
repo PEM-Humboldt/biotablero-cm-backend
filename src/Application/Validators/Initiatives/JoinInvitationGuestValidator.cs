569336e84a593c66bd87befe6191d966ef889448
cgalvis <cgalvis@humboldt.org.co>
2025-09-17 15:36:08 -0500

    feat: added  join invitation validators and specification

---
+++ src/Application/Validators/Initiatives/JoinInvitationGuestValidator.cs
@@ -0,0 +1,27 @@
+﻿namespace IAVH.BioTablero.CM.Application.Validators.Initiatives;
+
+using FluentValidation;
+
+using IAVH.BioTablero.CM.Application.DTOs.Initiatives;
+
+/// <summary>
+/// Join invitation guest validator.
+/// </summary>
+public class JoinInvitationGuestValidator : AbstractValidator<JoinInvitationGuestDto>
+{
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public JoinInvitationGuestValidator()
+    {
+        RuleFor(dto => dto)
+            .NotNull()
+                .WithMessage("Entity data cannot be null");
+
+        RuleFor(dto => dto.Email)
+            .NotEmpty()
+                .WithMessage("Email is required")
+            .EmailAddress()
+            .MaximumLength(100);
+    }
+}
