f9011ff0c043ef0949968dc74f51874a199cec61
cgalvis <cgalvis@humboldt.org.co>
2025-11-07 11:52:05 -0500

    feat: added migration for join request new level attribute

--- src/Infrastructure/Persistence/Migrations/20251107164442_AddJoinRequestUserlevel.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20251107164442_AddJoinRequestUserlevel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IAVH.BioTablero.CM.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IAVH.BioTablero.CM.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(GeneralContext))]
-    partial class GeneralContextModelSnapshot : ModelSnapshot
+    [Migration("20251107164442_AddJoinRequestUserlevel")]
+    partial class AddJoinRequestUserlevel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
