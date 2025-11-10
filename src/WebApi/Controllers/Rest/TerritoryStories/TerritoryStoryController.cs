83b42fc1ff600c5baeab720c19142551239f3d1c
cgalvis <cgalvis@humboldt.org.co>
2025-11-10 17:23:00 -0500

    feat: added territory story controllers

---
+++ src/WebApi/Controllers/Rest/TerritoryStories/TerritoryStoryController.cs
@@ -0,0 +1,114 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Controllers.Rest.TerritoryStories;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using IAVH.BioTablero.CM.Application.DTOs.TerritoryStories;
+using IAVH.BioTablero.CM.Application.Interfaces.Services;
+using IAVH.BioTablero.CM.Core.Domain.Entities.TerritoryStories;
+using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples;
+using IAVH.BioTablero.CM.WebApi.Interfaces;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+/// <summary>
+/// Territory Story controller.
+/// </summary>
+/// <param name="webTools">General web tools.</param>
+/// <param name="entityService">Entity service.</param>
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+[ApiConventionType(typeof(CustomApiConventions))]
+public class TerritoryStoryController(
+    IWebTools webTools,
+    ITerritoryStoryService entityService) : ODataController
+{
+    /// <summary>
+    /// Get entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Selected entity data.</returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetItem(int id, CancellationToken ct)
+    {
+        var response = await entityService.GetItemAsync(id, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Get entities (paginated).
+    /// </summary>
+    /// <param name="queryOptions">OData query options.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Entities list from parameters.</returns>
+    [HttpGet]
+    public async Task<IActionResult> GetOdataList(ODataQueryOptions<TerritoryStory> queryOptions, CancellationToken ct)
+    {
+        var response = await entityService.GetListAsync(queryOptions, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Add entity.
+    /// </summary>
+    /// <param name="requestData">Request data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Added entity data.</returns>
+    [HttpPut]
+    [Consumes("application/json")]
+    [Authorize]
+    public async Task<IActionResult> Put([FromBody] TerritoryStoryDto requestData, CancellationToken ct)
+    {
+        var response = await entityService.AddAsync(requestData, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Edit entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="requestData">Entity data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Updated entity data.</returns>
+    [HttpPost("{id}")]
+    [Consumes("application/json")]
+    [Authorize]
+    public async Task<IActionResult> Post(int id, [FromBody] TerritoryStoryDto requestData, CancellationToken ct)
+    {
+        var response = await entityService.UpdateAsync(id, requestData, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Enable entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Process result.</returns>
+    [HttpPost("Enable/{id}")]
+    [Authorize]
+    public async Task<IActionResult> Enable(int id, CancellationToken ct)
+    {
+        var response = await entityService.EnableAsync(id, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Disable entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Process result.</returns>
+    [HttpDelete("Disable/{id}")]
+    [Authorize]
+    public async Task<IActionResult> Disable(int id, CancellationToken ct)
+    {
+        var response = await entityService.DisableAsync(id, ct);
+        return webTools.CustomResponse(response);
+    }
+}
---
+++ src/WebApi/Controllers/Rest/TerritoryStories/TerritoryStoryVideoController.cs
@@ -0,0 +1,98 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Controllers.Rest.TerritoryStories;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using IAVH.BioTablero.CM.Application.DTOs.TerritoryStories;
+using IAVH.BioTablero.CM.Application.Interfaces.Services;
+using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples;
+using IAVH.BioTablero.CM.WebApi.Interfaces;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+/// <summary>
+/// Territory Story Video controller.
+/// </summary>
+/// <param name="webTools">General web tools.</param>
+/// <param name="entityService">Entity service.</param>
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+[ApiConventionType(typeof(CustomApiConventions))]
+public class TerritoryStoryVideoController(
+    IWebTools webTools,
+    ITerritoryStoryVideoService entityService) : ODataController
+{
+    /// <summary>
+    /// Get entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Selected entity data.</returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetItem(int id, CancellationToken ct)
+    {
+        var response = await entityService.GetItemAsync(id, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Get entities by Initiative.
+    /// </summary>
+    /// <param name="initiativeId">Initiative identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Entities list from parameters.</returns>
+    [HttpGet("GetByInitiative/{initiativeId}")]
+    public async Task<IActionResult> GetListByInitiative(int initiativeId, CancellationToken ct)
+    {
+        var response = await entityService.GetByInitiativeAsync(initiativeId, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Add entity.
+    /// </summary>
+    /// <param name="requestData">Request data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Added entity data.</returns>
+    [HttpPut]
+    [Consumes("application/json")]
+    [Authorize]
+    public async Task<IActionResult> Put([FromBody] TerritoryStoryVideoDto requestData, CancellationToken ct)
+    {
+        var response = await entityService.AddAsync(requestData, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Edit entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="requestData">Entity data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Updated entity data.</returns>
+    [HttpPost("{id}")]
+    [Consumes("application/json")]
+    [Authorize]
+    public async Task<IActionResult> Post(int id, [FromBody] TerritoryStoryVideoDto requestData, CancellationToken ct)
+    {
+        var response = await entityService.UpdateAsync(id, requestData, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Delete entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Process result.</returns>
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<IActionResult> Delete(int id, CancellationToken ct)
+    {
+        var response = await entityService.DeleteAsync(id, ct);
+        return webTools.CustomResponse(response);
+    }
+}
