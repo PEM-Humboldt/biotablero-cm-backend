66f32067e5fa29288d3a87e13debdf3567ecb4a0
cgalvis <cgalvis@humboldt.org.co>
2025-12-05 17:44:14 -0500

    fix: added initiative images extension

--- src/Application/Services/Initiatives/InitiativeService.cs
+++ src/Application/Services/Initiatives/InitiativeService.cs
@@ -381,7 +381,7 @@ public class InitiativeService : ServiceRead<Initiative, InitiativeDto, int>, II
 
         // Upload/Overwrite image
         var imageTypeStr = imageType.ToString("G").ToLower(CultureInfo.CurrentCulture);
-        var fileName = $"{StoragePrefix}/{id}/{imageTypeStr}";
+        var fileName = $"{StoragePrefix}/{id}/{imageTypeStr}.webp";
         var fileUri = new Uri($"{storageService.BaseUrl}/{fileName}");
         var uploadSuccessful = await storageService.UploadFileAsync(fileName, compressedImageStream, ContentTypes.ImageWebp, ct);
 
