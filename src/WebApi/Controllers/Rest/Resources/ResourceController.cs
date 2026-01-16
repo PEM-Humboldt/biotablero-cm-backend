f77b8cda9e5aa42655dec95b761bab17606b9a81
cgalvis <cgalvis@humboldt.org.co>
2026-01-16 16:47:18 -0500

    feat: added empty resource controller

---
+++ src/WebApi/Controllers/Rest/Resources/ResourceController.cs
@@ -0,0 +1,24 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Controllers.Rest.Resources;
+
+using System.ComponentModel.Design;
+
+using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples;
+using IAVH.BioTablero.CM.WebApi.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+/// <summary>
+/// Resource controller.
+/// </summary>
+/// <param name="webTools">General web tools.</param>
+/// <param name="entityService">Entity service.</param>
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+[ApiConventionType(typeof(CustomApiConventions))]
+public class ResourceController(
+    IWebTools webTools,
+    IResourceService entityService) : ODataController
+{
+}
