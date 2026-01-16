021a8ccf1087aefa1e0cfaee3a8900f71595ebe3
cgalvis <cgalvis@humboldt.org.co>
2026-01-16 17:14:39 -0500

    refactor: added code improvements in territory story service

--- src/Application/Services/TerritoryStory/TerritoryStoryService.cs
+++ src/Application/Services/TerritoryStory/TerritoryStoryService.cs
@@ -80,9 +80,9 @@ public class TerritoryStoryService : ServiceRead<TerritoryStory, TerritoryStoryD
     public async Task<CustomWebResponse> GetItemAsync(int id, string userName, CancellationToken ct = default)
     {
         // Validate user level and permissions
-        var entityExists = await entityRepository.AnyAsync(id, ct);
+        var entity = await entityRepository.GetByIdAsync(id, ct);
 
-        if (entityExists)
+        if (entity != null)
         {
             var authorizedUserAction = await entityRepository.AuthorizedEntityReadAsync(id, userName, ct);
 
@@ -93,31 +93,25 @@ public class TerritoryStoryService : ServiceRead<TerritoryStory, TerritoryStoryD
                     StatusCode = HttpStatusCode.Forbidden,
                 };
             }
-        }
 
-        var entity = await entityRepository.GetByIdAsync(id, ct);
-
-        if (entity != null)
-        {
             if (!string.IsNullOrWhiteSpace(userName))
             {
                 entity.ILikedIt = entity.Likes?.Any(e => e.UserName == userName);
             }
 
             var dataDto = mapper.Map(entity);
+
             return new()
             {
                 ResponseBody = dataDto,
             };
         }
-        else
+
+        return new(true)
         {
-            return new(true)
-            {
-                StatusCode = HttpStatusCode.NotFound,
-                Message = MessageConstants.NotFound,
-            };
-        }
+            StatusCode = HttpStatusCode.NotFound,
+            Message = MessageConstants.NotFound,
+        };
     }
 
     /// <inheritdoc/>
