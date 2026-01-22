dfe96d1ce4dfbf006d1e8119ae3686fe2d540b0b
cgalvis <cgalvis@humboldt.org.co>
2026-01-22 16:14:40 -0500

    feat: updated resource link controller

---
+++ src/WebApi/Config/DocsSetup/Examples/ResourceLink/ResourceLinkEditRequestExample.cs
@@ -0,0 +1,18 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.ResourceLink;
+
+using IAVH.BioTablero.CM.Application.DTOs.Resources;
+
+using Swashbuckle.AspNetCore.Filters;
+
+/// <summary>
+/// Resource Link add request example.
+/// </summary>
+public class ResourceLinkEditRequestExample : IExamplesProvider<ResourceLinkDto>
+{
+    /// <inheritdoc/>
+    public ResourceLinkDto GetExamples() => new()
+    {
+        Name = "Resource link example",
+        Url = "https://www.youtube.com/watch?v=I2Rz6cHdoHY",
+    };
+}
--- src/WebApi/Controllers/Rest/Resources/ResourceLinkController.cs
+++ src/WebApi/Controllers/Rest/Resources/ResourceLinkController.cs
@@ -3,11 +3,14 @@
 using System.Threading;
 using System.Threading.Tasks;
 
+using IAVH.BioTablero.CM.Application.DTOs.Resources;
 using IAVH.BioTablero.CM.Application.Interfaces.Services.Resources;
 using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples;
 using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.ResourceLink;
 using IAVH.BioTablero.CM.WebApi.Interfaces;
+using IAVH.BioTablero.CM.WebApi.Utils;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -51,6 +54,55 @@ public class ResourceLinkController(
     public async Task<IActionResult> GetListByResource(int resourceId, CancellationToken ct)
     {
         var response = await entityService.GetByResourceAsync(resourceId, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Add entity.
+    /// </summary>
+    /// <param name="requestData">Request data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Added entity data.</returns>
+    [HttpPut]
+    [Consumes("application/json")]
+    [Authorize]
+    [SwaggerRequestExample(typeof(ResourceLinkDto), typeof(ResourceLinkAddRequestExample))]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(ResourceLinkResponseExample))]
+    public async Task<IActionResult> Put([FromBody] ResourceLinkDto requestData, CancellationToken ct)
+    {
+        var response = await entityService.AddAsync(HttpContext.GetUserName(), requestData, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Edit entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="requestData">Entity data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Updated entity data.</returns>
+    [HttpPost("{id}")]
+    [Consumes("application/json")]
+    [Authorize]
+    [SwaggerRequestExample(typeof(ResourceLinkDto), typeof(ResourceLinkEditRequestExample))]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(ResourceLinkResponseExample))]
+    public async Task<IActionResult> Post(int id, [FromBody] ResourceLinkDto requestData, CancellationToken ct)
+    {
+        var response = await entityService.UpdateAsync(id, HttpContext.GetUserName(), requestData, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Delete entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Process result.</returns>
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<IActionResult> Delete(int id, CancellationToken ct)
+    {
+        var response = await entityService.DeleteAsync(id, HttpContext.GetUserName(), ct);
         return webTools.CustomResponse(response);
     }
 }
