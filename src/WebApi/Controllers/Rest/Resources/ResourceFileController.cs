aac7cce912329e94d2703f698a496b0a30d0a380
cgalvis <cgalvis@humboldt.org.co>
2026-01-22 16:08:10 -0500

    feat: updated resource file controller

--- src/WebApi/Controllers/Rest/Resources/ResourceFileController.cs
+++ src/WebApi/Controllers/Rest/Resources/ResourceFileController.cs
@@ -3,11 +3,16 @@
 using System.Threading;
 using System.Threading.Tasks;
 
+using IAVH.BioTablero.CM.Application.DTOs.Resources;
 using IAVH.BioTablero.CM.Application.Interfaces.Services.Resources;
+using IAVH.BioTablero.CM.Infrastructure.Integrations.Storage;
 using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples;
 using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.ResourceFile;
 using IAVH.BioTablero.CM.WebApi.Interfaces;
+using IAVH.BioTablero.CM.WebApi.Utils;
+using IAVH.BioTablero.CM.WebApi.Utils.Requests.ResourceFile;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -51,6 +56,62 @@ public class ResourceFileController(
     public async Task<IActionResult> GetListByResource(int resourceId, CancellationToken ct)
     {
         var response = await entityService.GetByResourceAsync(resourceId, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Add entity.
+    /// </summary>
+    /// <param name="requestData">Request data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Added entity data.</returns>
+    [HttpPut]
+    [Authorize]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(ResourceFileResponseExample))]
+    public async Task<IActionResult> Put([FromForm] ResourceFileAddRequest requestData, CancellationToken ct)
+    {
+        var requestDataDto = new ResourceFileDto()
+        {
+            ResourceId = requestData.ResourceId,
+            Name = requestData.Name,
+        };
+
+        var response = await entityService.AddAsync(HttpContext.GetUserName(), requestDataDto, new FormFileAdapter(requestData.File), ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Edit entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="requestData">Entity data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Updated entity data.</returns>
+    [HttpPost("{id}")]
+    [Authorize]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(ResourceFileResponseExample))]
+    public async Task<IActionResult> Post(int id, [FromForm] ResourceFileEditRequest requestData, CancellationToken ct)
+    {
+        var requestDataDto = new ResourceFileDto()
+        {
+            Name = requestData.Name,
+        };
+
+        var response = await entityService.UpdateAsync(id, HttpContext.GetUserName(), requestDataDto, new FormFileAdapter(requestData.File), ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Delete entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Process result.</returns>
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<IActionResult> Delete(int id, CancellationToken ct)
+    {
+        var response = await entityService.DeleteAsync(id, HttpContext.GetUserName(), ct);
         return webTools.CustomResponse(response);
     }
 }
---
+++ src/WebApi/Utils/Requests/ResourceFile/ResourceFileAddRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Utils.Requests.ResourceFile;
+
+/// <summary>
+/// Resource file add request.
+/// </summary>
+public class ResourceFileAddRequest : ResourceFileEditRequest
+{
+    /// <summary>
+    /// Resource identifier.
+    /// </summary>
+    public int ResourceId { get; set; }
+}
