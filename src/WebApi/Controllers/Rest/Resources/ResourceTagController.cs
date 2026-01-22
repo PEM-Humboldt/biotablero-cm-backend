ac0d9c6541463e8ceccac48b4733acea0730e2a1
cgalvis <cgalvis@humboldt.org.co>
2026-01-22 15:53:30 -0500

    feat: added resource tag controller

---
+++ src/WebApi/Controllers/Rest/Resources/ResourceTagController.cs
@@ -0,0 +1,59 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Controllers.Rest.Resources;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using IAVH.BioTablero.CM.Application.Interfaces.Services.Resources;
+using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples;
+using IAVH.BioTablero.CM.WebApi.Interfaces;
+using IAVH.BioTablero.CM.WebApi.Utils;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Resource Tag controller.
+/// </summary>
+/// <param name="webTools">General web tools.</param>
+/// <param name="entityService">Entity service.</param>
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+[ApiConventionType(typeof(CustomApiConventions))]
+public class ResourceTagController(
+    IWebTools webTools,
+    IResourceTagService entityService) : ControllerBase
+{
+    /// <summary>
+    /// Add entity.
+    /// </summary>
+    /// <param name="resourceId">Resource identifier.</param>
+    /// <param name="tagId">Tag identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Added entity data.</returns>
+    [HttpPut]
+    [Authorize]
+    [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Put(int resourceId, int tagId, CancellationToken ct)
+    {
+        var response = await entityService.AddAsync(HttpContext.GetUserName(), resourceId, tagId, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Delete entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Process result.</returns>
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<IActionResult> Delete(int id, CancellationToken ct)
+    {
+        var response = await entityService.DeleteAsync(id, HttpContext.GetUserName(), ct);
+        return webTools.CustomResponse(response);
+    }
+}
