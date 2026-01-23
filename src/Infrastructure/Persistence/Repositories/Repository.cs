8ef0c1bb0000aa5e7f41fc349ad88b529c17e620
cgalvis <cgalvis@humboldt.org.co>
2026-01-23 12:26:34 -0500

    feat: updated general repository with virtual methods

--- src/Infrastructure/Persistence/Repositories/Repository.cs
+++ src/Infrastructure/Persistence/Repositories/Repository.cs
@@ -50,19 +50,19 @@ public class Repository<TE, TI> : IRepository<TE, TI>
     }
 
     /// <inheritdoc/>
-    public async Task<TE> GetByIdAsync(TI id, CancellationToken ct = default) => await dbContext.Set<TE>().FindAsync([id], ct);
+    public virtual async Task<TE> GetByIdAsync(TI id, CancellationToken ct = default) => await dbContext.Set<TE>().FindAsync([id], ct);
 
     /// <inheritdoc/>
-    public async Task<List<TE>> ListAsync(CancellationToken ct = default) => await dbContext.Set<TE>().ToListAsync(ct);
+    public virtual async Task<List<TE>> ListAsync(CancellationToken ct = default) => await dbContext.Set<TE>().ToListAsync(ct);
 
     /// <inheritdoc/>
-    public async Task<int> CountAsync(CancellationToken ct = default) => await dbContext.Set<TE>().CountAsync(ct);
+    public virtual async Task<int> CountAsync(CancellationToken ct = default) => await dbContext.Set<TE>().CountAsync(ct);
 
     /// <inheritdoc/>
-    public async Task<bool> AnyAsync(CancellationToken ct = default) => await dbContext.Set<TE>().AnyAsync(ct);
+    public virtual async Task<bool> AnyAsync(CancellationToken ct = default) => await dbContext.Set<TE>().AnyAsync(ct);
 
     /// <inheritdoc/>
-    public async Task<bool> AnyAsync(TI id, CancellationToken ct = default)
+    public virtual async Task<bool> AnyAsync(TI id, CancellationToken ct = default)
     {
         var parameter = Expression.Parameter(typeof(TE), "e");
         var property = Expression.Property(parameter, "Id");
@@ -75,7 +75,7 @@ public class Repository<TE, TI> : IRepository<TE, TI>
     }
 
     /// <inheritdoc/>
-    public async Task<TE> AddAsync(TE entity, CancellationToken ct = default)
+    public virtual async Task<TE> AddAsync(TE entity, CancellationToken ct = default)
     {
         dbContext.Set<TE>().Add(entity);
         await SaveChangesAsync(ct);
@@ -83,7 +83,7 @@ public class Repository<TE, TI> : IRepository<TE, TI>
     }
 
     /// <inheritdoc/>
-    public async Task<IEnumerable<TE>> AddRangeAsync(IEnumerable<TE> entities, CancellationToken ct = default)
+    public virtual async Task<IEnumerable<TE>> AddRangeAsync(IEnumerable<TE> entities, CancellationToken ct = default)
     {
         dbContext.Set<TE>().AddRange(entities);
         await SaveChangesAsync(ct);
@@ -91,7 +91,7 @@ public class Repository<TE, TI> : IRepository<TE, TI>
     }
 
     /// <inheritdoc/>
-    public async Task<int> UpdateAsync(TE entity, CancellationToken ct = default)
+    public virtual async Task<int> UpdateAsync(TE entity, CancellationToken ct = default)
     {
         dbContext.Entry(entity).State = EntityState.Modified;
         var result = await SaveChangesAsync(ct);
@@ -99,7 +99,7 @@ public class Repository<TE, TI> : IRepository<TE, TI>
     }
 
     /// <inheritdoc/>
-    public async Task<int> UpdateRangeAsync(IEnumerable<TE> entities, CancellationToken ct = default)
+    public virtual async Task<int> UpdateRangeAsync(IEnumerable<TE> entities, CancellationToken ct = default)
     {
         foreach (var entity in entities)
         {
@@ -111,7 +111,7 @@ public class Repository<TE, TI> : IRepository<TE, TI>
     }
 
     /// <inheritdoc/>
-    public async Task<int> DeleteAsync(TE entity, CancellationToken ct = default)
+    public virtual async Task<int> DeleteAsync(TE entity, CancellationToken ct = default)
     {
         dbContext.Set<TE>().Remove(entity);
         var result = await SaveChangesAsync(ct);
@@ -119,7 +119,7 @@ public class Repository<TE, TI> : IRepository<TE, TI>
     }
 
     /// <inheritdoc/>
-    public async Task<int> DeleteRangeAsync(IEnumerable<TE> entities, CancellationToken ct = default)
+    public virtual async Task<int> DeleteRangeAsync(IEnumerable<TE> entities, CancellationToken ct = default)
     {
         dbContext.Set<TE>().RemoveRange(entities);
         var result = await SaveChangesAsync(ct);
@@ -127,20 +127,20 @@ public class Repository<TE, TI> : IRepository<TE, TI>
     }
 
     /// <inheritdoc/>
-    public async Task<int> SaveChangesAsync(CancellationToken ct = default) => await dbContext.SaveChangesAsync(ct);
+    public virtual async Task<int> SaveChangesAsync(CancellationToken ct = default) => await dbContext.SaveChangesAsync(ct);
 
     /// <inheritdoc/>
     public IQueryable<TE> GetQueryable() =>
         dbContext.Set<TE>().AsNoTracking();
 
     /// <inheritdoc/>
-    public async Task<int> QueryCountAsync(IQueryable<TE> query, CancellationToken ct = default) => await query.CountAsync(ct);
+    public virtual async Task<int> QueryCountAsync(IQueryable<TE> query, CancellationToken ct = default) => await query.CountAsync(ct);
 
     /// <inheritdoc/>
-    public async Task<List<TE>> QueryToListAsync(IQueryable<TE> query, CancellationToken ct = default) => await query.ToListAsync(ct);
+    public virtual async Task<List<TE>> QueryToListAsync(IQueryable<TE> query, CancellationToken ct = default) => await query.ToListAsync(ct);
 
     /// <inheritdoc/>
-    public async Task<TR> ExecuteInTransactionAsync<TR>(Func<CancellationToken, Task<TR>> action, string errorContext, CancellationToken ct = default)
+    public virtual async Task<TR> ExecuteInTransactionAsync<TR>(Func<CancellationToken, Task<TR>> action, string errorContext, CancellationToken ct = default)
     {
         using var transaction = await dbContext.Database.BeginTransactionAsync(ct);
 
