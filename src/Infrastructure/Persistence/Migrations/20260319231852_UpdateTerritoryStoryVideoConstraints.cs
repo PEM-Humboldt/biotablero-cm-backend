1c8ed1e95633f1173c85c716a8f676b8f5ada3ef
cgalvis <cgalvis@humboldt.org.co>
2026-03-19 18:24:03 -0500

    feat: added migration with updated territory story video constraints

---
+++ src/Infrastructure/Persistence/Migrations/20260319231852_UpdateTerritoryStoryVideoConstraints.cs
@@ -0,0 +1,52 @@
+﻿#nullable disable
+
+namespace IAVH.BioTablero.CM.Infrastructure.Persistence.Migrations;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateTerritoryStoryVideoConstraints : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "IX_territory_story_video_file_url",
+            schema: "initiatives",
+            table: "territory_story_video");
+
+        migrationBuilder.DropIndex(
+            name: "IX_territory_story_video_territory_story_id",
+            schema: "initiatives",
+            table: "territory_story_video");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_territory_story_video_territory_story_id_file_url",
+            schema: "initiatives",
+            table: "territory_story_video",
+            columns: new[] { "territory_story_id", "file_url" },
+            unique: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "IX_territory_story_video_territory_story_id_file_url",
+            schema: "initiatives",
+            table: "territory_story_video");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_territory_story_video_file_url",
+            schema: "initiatives",
+            table: "territory_story_video",
+            column: "file_url",
+            unique: true);
+
+        migrationBuilder.CreateIndex(
+            name: "IX_territory_story_video_territory_story_id",
+            schema: "initiatives",
+            table: "territory_story_video",
+            column: "territory_story_id");
+    }
+}
