5f7590d7491db73986002e613064673241855bfe
cgalvis <cgalvis@humboldt.org.co>
2026-05-22 10:56:16 -0500

    feat: added validation for initiative stats endpoint

--- src/Application/Services/Statistics/InitiativeStatsService.cs
+++ src/Application/Services/Statistics/InitiativeStatsService.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 
 using IAVH.BioTablero.CM.Application.Domain;
 using IAVH.BioTablero.CM.Application.DTOs.Reports;
+using IAVH.BioTablero.CM.Application.Interfaces.General;
 using IAVH.BioTablero.CM.Application.Interfaces.Services.Statistics;
+using IAVH.BioTablero.CM.Core.Domain.Models.Validations;
 using IAVH.BioTablero.CM.Core.Interfaces.Repositories.Indicators;
 using IAVH.BioTablero.CM.Core.Interfaces.Repositories.Initiatives;
 using IAVH.BioTablero.CM.Core.Interfaces.Repositories.Locations;
@@ -13,17 +15,30 @@ using IAVH.BioTablero.CM.Core.Interfaces.Repositories.Locations;
 /// <summary>
 /// Initiative statistics service.
 /// </summary>
+/// <param name="errorTranslator">Error translator.</param>
 /// <param name="entityRepository">Monitoring Events repository.</param>
 /// <param name="indicatorRepository">Indicator repository.</param>
 /// <param name="locationRepository">Location repository.</param>
+/// <param name="initiativeRepository">Initiative repository.</param>
 public class InitiativeStatsService(
+    IValidationErrorTranslator errorTranslator,
     IMonitoringEventsRepository entityRepository,
     IIndicatorRepository indicatorRepository,
-    ILocationRepository locationRepository) : IInitiativeStatsService
+    ILocationRepository locationRepository,
+    IInitiativeRepository initiativeRepository) : IInitiativeStatsService
 {
     /// <inheritdoc/>
     public async Task<CustomWebResponse> GetStats(int initiativeId, CancellationToken ct = default)
     {
+        // Validate initiative
+        if (!await initiativeRepository.AnyAsync(initiativeId, ct))
+        {
+            return new(true)
+            {
+                ResponseBody = errorTranslator.Translate(ValidationErrorCodes.General.ElementNotFound),
+            };
+        }
+
         var totalMunicipalities = await locationRepository.GetMunicipalitiesCountAsync(initiativeId, ct);
 
         return new()
