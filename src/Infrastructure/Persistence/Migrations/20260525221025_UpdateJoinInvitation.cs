d756337b767c1ab46919684eab95a88ce229466e
cgalvis <cgalvis@humboldt.org.co>
2026-05-25 17:20:22 -0500

    feat: added migration

---
+++ src/Infrastructure/Persistence/Migrations/20260525221025_UpdateJoinInvitation.cs
@@ -0,0 +1,24 @@
+﻿#nullable disable
+
+namespace IAVH.BioTablero.CM.Infrastructure.Persistence.Migrations;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateJoinInvitation : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.AddColumn<string>(
+            name: "html_message",
+            schema: "initiatives",
+            table: "join_invitation",
+            type: "text",
+            nullable: false,
+            defaultValue: string.Empty);
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropColumn(
+            name: "html_message",
+            schema: "initiatives",
+            table: "join_invitation");
+}
