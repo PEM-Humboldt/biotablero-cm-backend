092888fda5c051f48d35aebbe07dcbe1724fa8d6
cgalvis <cgalvis@humboldt.org.co>
2026-05-26 18:00:58 -0500

    fix: solved issue with indicator type relationship

--- src/Infrastructure/Persistence/Config/Entities/Indicators/IndicatorConfig.cs
+++ src/Infrastructure/Persistence/Config/Entities/Indicators/IndicatorConfig.cs
@@ -35,6 +35,6 @@ public class IndicatorConfig : IEntityTypeConfiguration<Indicator>
 
         builder.HasOne(e => e.Type)
             .WithMany(p => p.Indicators)
-            .HasForeignKey(e => e.InitiativeId);
+            .HasForeignKey(e => e.IndicatorTypeId);
     }
 }
