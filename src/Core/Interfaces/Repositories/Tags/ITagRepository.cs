f10c200fd6db7cdf96e262df9e584a96592764e3
cgalvis <cgalvis@humboldt.org.co>
2026-07-08 12:35:17 -0500

    feat: updated duplicated validations for Tag and InitiativeLocation entities

--- src/Core/Interfaces/Repositories/Tags/ITagRepository.cs
+++ src/Core/Interfaces/Repositories/Tags/ITagRepository.cs
@@ -11,13 +11,13 @@ using IAVH.BioTablero.CM.Core.Domain.Entities.Tags;
 public interface ITagRepository : IRepository<Tag, int>
 {
     /// <summary>
-    /// Get if elements exists by name and category.
+    /// Get if element is duplicated.
     /// </summary>
     /// <param name="name">Entity name.</param>
     /// <param name="categoryId">Tag Category identifier.</param>
     /// <param name="ct">Cancellation token.</param>
     /// <returns>True if any element exists. False otherwise.</returns>
-    Task<bool> AnyByNameAndCategory(string name, int categoryId, CancellationToken ct = default);
+    Task<bool> IsDuplicated(string name, int categoryId, CancellationToken ct = default);
 
     /// <summary>
     /// Get if element is duplicated.
