2814c8f1378c8439e8b250208da803c5ab5f9a98
cgalvis <cgalvis@humboldt.org.co>
2026-07-10 11:24:20 -0500

    feat: updated healthchecks

--- src/WebApi/Config/DependencyRegistry/ConfigCoreDependencies.cs
+++ src/WebApi/Config/DependencyRegistry/ConfigCoreDependencies.cs
@@ -30,6 +30,7 @@ public static class ConfigCoreDependencies
 {
     private static readonly string OidcServer = $"{Environment.GetEnvironmentVariable("KC_BASE_URL")}/realms/{Environment.GetEnvironmentVariable("KC_REALM")}/";
     private static readonly string ConnectionString = Environment.GetEnvironmentVariable("CS_MAIN");
+    private static readonly Uri IamCustomApiHealthUrl = new($"{Environment.GetEnvironmentVariable("KC_CUSTOM_API_HOST")}/health/ready");
     private static readonly SmtpConfigData SmtpData = EmailService.InitSmtpData();
 
     /// <summary>
@@ -127,6 +128,11 @@ public static class ConfigCoreDependencies
                 oidcSvrUri: new Uri(OidcServer),
                 name: "keycloak",
                 tags: ["ready"])
+            .AddUrlGroup(
+                uri: IamCustomApiHealthUrl,
+                name: "keycloak-custom-api",
+                tags: ["ready"],
+                timeout: TimeSpan.FromSeconds(5))
             .AddNpgSql(
                 ConnectionString,
                 name: "postgres",
