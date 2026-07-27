659af136edaa4bb9452826bd2f5fce43b95323d2
cgalvis <cgalvis@humboldt.org.co>
2026-07-27 09:44:41 -0500

    refactor: added services folder for reports

--- src/Infrastructure/Integrations/Reports/Services/ReportExcelService.cs
+++ src/Infrastructure/Integrations/Reports/Services/ReportExcelService.cs
@@ -1,4 +1,4 @@
-﻿namespace IAVH.BioTablero.CM.Infrastructure.Integrations.Reports;
+﻿namespace IAVH.BioTablero.CM.Infrastructure.Integrations.Reports.Services;
 
 using System;
 using System.Collections.Generic;
@@ -10,7 +10,7 @@ using System.Reflection;
 using ClosedXML.Excel;
 
 using IAVH.BioTablero.CM.Application.DTOs.Reports;
-using IAVH.BioTablero.CM.Application.Interfaces.ExternalServices.Reports;
+using IAVH.BioTablero.CM.Application.Interfaces.ExternalServices.Reports.Services;
 using IAVH.BioTablero.CM.Application.Interfaces.General;
 using IAVH.BioTablero.CM.Infrastructure.Integrations.Reports.Config.General;
 using IAVH.BioTablero.CM.Infrastructure.Integrations.Reports.Interfaces;
