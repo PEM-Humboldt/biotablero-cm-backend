9da5bd79fb6bbfc58ac328651e44e737141c07a0
cgalvis <cgalvis@humboldt.org.co>
2026-08-06 10:24:31 -0500

    feat: added report data entity

---
+++ src/Core/Domain/Entities/Reports/ReportData.cs
@@ -0,0 +1,32 @@
+﻿namespace IAVH.BioTablero.CM.Core.Domain.Entities.Reports;
+
+using System;
+
+using IAVH.BioTablero.CM.Core.Domain.Entities;
+using IAVH.BioTablero.CM.Core.Interfaces.Entities;
+
+/// <summary>
+/// Report Data entity.
+/// </summary>
+public class ReportData : BaseEntity<int>, IAggregateRoot
+{
+    /// <summary>
+    /// Report creation date.
+    /// </summary>
+    public DateTimeOffset CreationDate { get; set; }
+
+    /// <summary>
+    /// Creator user name.
+    /// </summary>
+    public string UserName { get; set; }
+
+    /// <summary>
+    /// Report generation description.
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Report generation data.
+    /// </summary>
+    public string Data { get; set; }
+}
