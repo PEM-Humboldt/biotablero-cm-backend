49e08b4290b1b3cf053affb92e857a159d2f1195
cgalvis <cgalvis@humboldt.org.co>
2026-08-06 10:48:30 -0500

    feat: added report data controller and swagger examples

---
+++ src/WebApi/Config/DocsSetup/Examples/Reports/ReportDataAddRequestExample.cs
@@ -0,0 +1,19 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.Reports;
+
+using IAVH.BioTablero.CM.Application.DTOs.Reports;
+
+using Swashbuckle.AspNetCore.Filters;
+
+/// <summary>
+/// Report Data add request example.
+/// </summary>
+public class ReportDataAddRequestExample : IExamplesProvider<ReportDataDto>
+{
+    /// <inheritdoc/>
+    public ReportDataDto GetExamples() => new()
+    {
+        UserName = "user-example@example.com",
+        Description = "Description example",
+        Data = "Data example",
+    };
+}
