59ca17bb94cc612c3ce4374cf19ac3387865d5d0
cgalvis <cgalvis@humboldt.org.co>
2026-08-06 11:08:45 -0500

    feat: added migration

---
+++ src/Infrastructure/Persistence/Migrations/20260806155847_AddReportsData.cs
@@ -0,0 +1,42 @@
+﻿#nullable disable
+
+namespace IAVH.BioTablero.CM.Infrastructure.Persistence.Migrations;
+
+using System;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+/// <inheritdoc />
+public partial class AddReportsData : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.EnsureSchema(
+            name: "reports");
+
+        migrationBuilder.CreateTable(
+            name: "report_data",
+            schema: "reports",
+            columns: table => new
+            {
+                id = table.Column<int>(type: "integer", nullable: false)
+                    .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                creation_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                user_name = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: false),
+                description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                data = table.Column<string>(type: "character varying(280)", maxLength: 280, nullable: true),
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_report_data", x => x.id);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropTable(
+            name: "report_data",
+            schema: "reports");
+}
