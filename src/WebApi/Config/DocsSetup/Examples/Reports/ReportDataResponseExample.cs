d66f3d5ec6117215295dcd2e2e780e406661ea3e
cgalvis <cgalvis@humboldt.org.co>
2026-08-06 10:48:30 -0500

    feat: added report data controller and swagger examples

---
+++ src/WebApi/Config/DocsSetup/Examples/Reports/ReportDataResponseExample.cs
@@ -0,0 +1,23 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.Reports;
+
+using System;
+
+using IAVH.BioTablero.CM.Application.DTOs.Reports;
+
+using Swashbuckle.AspNetCore.Filters;
+
+/// <summary>
+/// Report Data response example.
+/// </summary>
+public class ReportDataResponseExample : IExamplesProvider<ReportDataDto>
+{
+    /// <inheritdoc/>
+    public ReportDataDto GetExamples() => new()
+    {
+        Id = 0,
+        UserName = "user-example@example.com",
+        CreationDate = DateTimeOffset.UtcNow,
+        Description = "Description example",
+        Data = "Data example",
+    };
+}
---
+++ src/WebApi/Controllers/Rest/Reports/ReportsController.cs
@@ -0,0 +1,82 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Controllers.Rest.Reports;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using IAVH.BioTablero.CM.Application.DTOs.Reports;
+using IAVH.BioTablero.CM.Application.Interfaces.Services.Reports;
+using IAVH.BioTablero.CM.Core.Domain.Entities.Reports;
+using IAVH.BioTablero.CM.Core.Domain.Utils.Constants;
+using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples;
+using IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.Reports;
+using IAVH.BioTablero.CM.WebApi.Interfaces;
+using IAVH.BioTablero.CM.WebApi.Utils;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+using Swashbuckle.AspNetCore.Filters;
+
+/// <summary>
+/// Reports controller.
+/// </summary>
+/// <param name="webTools">General web tools.</param>
+/// <param name="entityService">Entity service.</param>
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+[ApiConventionType(typeof(CustomApiConventions))]
+public class ReportsController(
+    IWebTools webTools,
+    IReportDataService entityService) : ControllerBase
+{
+    /// <summary>
+    /// Get entity.
+    /// </summary>
+    /// <param name="id">Entity identifier.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Selected entity data.</returns>
+    [HttpGet("{id}")]
+    [Authorize(Roles = IamConstants.RoleModuleAdmin)]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(ReportDataResponseExample))]
+    public async Task<IActionResult> GetItem(int id, CancellationToken ct)
+    {
+        var response = await entityService.GetItemAsync(id, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Get entities (paginated).
+    /// </summary>
+    /// <param name="queryOptions">OData query options.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Entities list from parameters.</returns>
+    [HttpGet]
+    [Authorize(Roles = IamConstants.RoleModuleAdmin)]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(ReportDataOdataResponseExample))]
+    public async Task<IActionResult> GetOdataList(ODataQueryOptions<ReportData> queryOptions, CancellationToken ct)
+    {
+        var response = await entityService.GetListAsync(queryOptions, ct);
+        return webTools.CustomResponse(response);
+    }
+
+    /// <summary>
+    /// Add entity.
+    /// </summary>
+    /// <param name="requestData">Request data.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Added entity data.</returns>
+    [HttpPost]
+    [Consumes("application/json")]
+    [Authorize]
+    [SwaggerRequestExample(typeof(ReportDataDto), typeof(ReportDataAddRequestExample))]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(ReportDataResponseExample))]
+    public async Task<IActionResult> Post([FromBody] ReportDataDto requestData, CancellationToken ct)
+    {
+        requestData.UserName = HttpContext.GetUserName();
+        var response = await entityService.AddAsync(requestData, ct);
+        return webTools.CustomResponse(response);
+    }
+}
