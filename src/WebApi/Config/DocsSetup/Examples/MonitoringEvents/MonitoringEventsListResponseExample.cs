29363bcf0d27d16defb6dd55b1b635be0dd5f825
cgalvis <cgalvis@humboldt.org.co>
2026-09-01 10:22:53 -0500

    feat: added controller and openapi examples for monitoring events

---
+++ src/WebApi/Config/DocsSetup/Examples/MonitoringEvents/MonitoringEventsListResponseExample.cs
@@ -0,0 +1,26 @@
+﻿namespace IAVH.BioTablero.CM.WebApi.Config.DocsSetup.Examples.MonitoringEvents;
+
+using System;
+using System.Collections.Generic;
+
+using IAVH.BioTablero.CM.Application.DTOs.Initiatives;
+
+using IAVH.BioTablero.CM.WebApi.Interfaces;
+
+/// <summary>
+/// Monitoring Events list response example.
+/// </summary>
+public class MonitoringEventsListResponseExample : IOpenApiExampleProvider<List<MonitoringEventsDto>>
+{
+    /// <inheritdoc/>
+    public List<MonitoringEventsDto> GetExamples() =>
+    [
+        new()
+        {
+            Id = 0,
+            InitiativeId = 0,
+            Date = new DateTimeOffset(2026, 1, 1, 0, 0, 0, default),
+            Value = 1,
+        },
+    ];
+}
